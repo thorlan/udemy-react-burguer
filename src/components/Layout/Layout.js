779e480db1195af3c34cf573e1e7a7b4299453f5
thiago orlandini <thiagorlandini@hotmail.com>
2020-03-29 12:51:18 -0300

    teste

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Aux from '../../hoc/Aux';
+import classes from './Layout.css'
+
+const layout = (props) => (
+
+    <Aux>
+        <div>Toolbar, sidedrawer, backdrop</div>
+        <main className={classes.content}>
+            {props.children}
+        </main>
+    </Aux>
+);
+
+export default layout;
