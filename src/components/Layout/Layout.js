aa3af4f64cc97e85531648c4a2d00cc7e61e7030
thiago orlandini <thiagorlandini@hotmail.com>
2020-03-29 13:38:55 -0300

    burgerIngredient

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 import Aux from '../../hoc/Aux';
-import classes from './Layout.css'
+
+import classes from './layout.module.css'
 
 const layout = (props) => (
 
     <Aux>
         <div>Toolbar, sidedrawer, backdrop</div>
+        
         <main className={classes.content}>
             {props.children}
         </main>
+        
     </Aux>
 );
 
